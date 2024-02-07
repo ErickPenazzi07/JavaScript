11462c710962c5aeb98aa02677de6779242a45fa
Erick <penazzi1234@gmail.com>
2024-02-07 17:03:16 -0300

    Finalizado atividade sala de auala

---
+++ somandotexto.js
@@ -0,0 +1,10 @@
+//var nome = "DjErick";
+//var boavindas = "Olá " + nome + ", seja bem-vindo.";
+
+//console.log(boavindas);
+
+var texto1 = "Olá"
+var texto2 = "Bom dia"
+
+console.log(texto1 + texto2)
+console.log(texto1+", "+ texto2)
