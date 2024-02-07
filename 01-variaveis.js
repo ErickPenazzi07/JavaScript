5324f32624f6aaa64f2d3bd9476a1b662cc38012
Erick <penazzi1234@gmail.com>
2024-02-07 11:26:55 -0300

    Java Script comandos basicos

---
+++ 01-variaveis.js
@@ -0,0 +1,11 @@
+//Criando variáveis com "var" (var nome_variavel = <valor>;)
+
+var nome = "Djerick"; //var cria variavel global
+var idade = 16
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos!");
+console.log ("Agora fiz aniversário!🥳 Tenho", idade + 1, "anos!");
+
+//aumentando automagicamnte +1 (autoicremento)
+idade++ //idade = idade + 1;
+console.log ("Agora fiz aniversário!🥳 Tenho", idade, "anos!");
---
+++ 02 aritmetico.js
@@ -0,0 +1,22 @@
+//utilizando operadores aitmeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+var X = 9;
+var Y = 7;
+
+console.log("A soma entre", X, "e", Y, "é", X + Y);
+
+var soma = X = Y;
+var subtracao = X - Y;
+var multiplicacao = X * Y
+var divisao = X / Y;
+console.log("") //Outra forma de pular linha
+
+console.log("Soma =", soma)
+console.log("subtracao =", subtracao)
+console.log("multiplicacao =", multiplicacao)
+console.log("divisao =", divisao)
+
+//Regras para nomear identificadores (var, const ou let)
+//Podem começar com A-Z, a-z, _ ou $
