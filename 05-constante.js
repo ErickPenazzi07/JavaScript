8327250a0018411a807fa08e9dfbc73605ef3113
Erick <penazzi1234@gmail.com>
2024-02-20 14:52:33 -0300

    JavaScript

---
+++ 05-constante.js
@@ -0,0 +1,18 @@
+// const x = 10;
+// var y = 20
+
+// console.log(x+y)
+
+// y=3
+// console.log(x+y)
+
+//Criando constante com "const" (const nome_constante = <valor> )
+//Atividade de cálculo de do comprimento de um circuferência
+
+const pi = 3.14159265359
+var raio = 5
+
+console.log("Comprimento de uma circunferência de raio " + raio + " cm:", 2*pi*raio)
+
+console.log("Tentando altera o valor de pi!")
+//pi=10
