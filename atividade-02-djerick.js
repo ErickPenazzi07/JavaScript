34a6a4344480cc57e9256b064d300bd7681a3770
Erick <penazzi1234@gmail.com>
2024-02-20 14:56:16 -0300

    Atividade 02

--- atividade-02-djerick.js
+++ atividade-02-djerick.js
@@ -1,6 +1,7 @@
 var texto = "DjErick";
 var dataNascimento = 2007;
 var dataAtual = 2024;
+
 console.log("----------Atividade 01---------");
 console.log(texto, "Nasceu em ", dataNascimento);
 
@@ -11,15 +12,12 @@ var comp = 12;
 var lar = 6;
 var alt = 5;
 
+console.log("");
 console.log("---------Atividade 02--------");
-console.log(
-  "A área de um retângulo que tem 12 cm de comprimento e 6 cm de largura, qual sua área?"
-);
+console.log("Resultado:", comp * lar, "cm²");
 console.log("");
+
 console.log("---------Atividade 03--------");
-console.log("Resultado:", comp * lar, "cm");
-console.log("");
-console.log("---------Atividade 04--------");
 console.log(
   "Se adicionarmos uma altura de 5 cm o volume será:",
   comp * lar * alt,
@@ -32,16 +30,18 @@ var a = 2;
 var b = 3;
 var c = -1;
 
-console.log("O Delta será:", b ^ (2 - 4 * a * c));
+console.log("O Delta será:", b^2 - 4 * a * c);
 console.log("");
 console.log("---------Atividade 05--------");
 
 var preço = 450;
-var desco = 13.5
+var desco = 13.5;
 console.log(
   "O desconto será R$:",
   (preço / 100) * 3,
-  "e o valor a ser pago irá ser R$:", preço-desco);
+  "e o valor a ser pago irá ser R$:",
+  preço - desco
+);
 console.log("");
 console.log("---------Atividade 06--------");
 var time = 3.5;
